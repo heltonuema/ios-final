fa0e4e81a8439c010bd3533f97ba316ef55389df
Helton <heltonuema@gmail.com>
2016-10-27 21:06:03 -0200

    Navegação entre as três cenas

--- iosfinal/MainTableViewController.m
+++ iosfinal/MainTableViewController.m
@@ -16,12 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    UIBarButtonItem * addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    
+    self.navigationItem.rightBarButtonItem = addButton;
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)insertNewObject:(id)sender{
+    [self performSegueWithIdentifier:@"showEntrega" sender:sender];
 }
 
 - (void)didReceiveMemoryWarning {
